30458f46a3164b53057d19b0cdeeb96872f93943
Jonathan Garcia <jonathan.garcia@awsoftware.com.ar>
2018-01-31 17:57:46 -0300

    - Agregado Terminos y condiciones

---
+++ OaoApplication/TerminosCondicionesViewController.h
@@ -0,0 +1,15 @@
+//
+//  TerminosCondicionesViewController.h
+//  OaoApplication
+//
+//  Created by Jonathan Garcia on 31/1/18.
+//  Copyright © 2018 Desarrollo AWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TerminosCondicionesViewController : UIViewController
+
++ (TerminosCondicionesViewController * )getVcId;
+
+@end
